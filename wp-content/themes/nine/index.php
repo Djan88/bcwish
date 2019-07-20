f605b6876cc1393481ec4ebce0de935440b1e3a0
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-07-20 13:08:07 +0300

    nine-45

--- wp-content/themes/nine/index.php
+++ wp-content/themes/nine/index.php
@@ -165,14 +165,14 @@
     <?php } ?>
     <section id="about">
       <div class="box">
-        <h2 class="section-title">Who is Behind All This</h2>
+        <h2 class="section-title">Что такое "Девяточка"?</h2>
         <div class="row">
 	      <div class="col-md-5 col-md-push-7 col-sm-12">
             <figure class="frame"><img src="<?php bloginfo('template_url'); ?>/style/images/art/nine.jpg" alt="" /></figure>
           </div>
           <!-- /column -->
           <div class="col-md-7 col-md-pull-5 col-sm-12">
-            <p class="lead">Nulla vitae elit libero, a pharetra augue. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna. </p>
+            <p class="lead">Психодинамическая трансформационная игра для освобождения телесного подсознания</p>
             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Fusce dapibus, tellus. </p>
             <p>Tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
             <p>Fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Etiam porta sem malesuada magna mollis euismod. Aenean lacinia bibendum nulla.</p>
@@ -181,81 +181,32 @@
         </div>
         <!-- /.row -->
         <div class="clearfix"></div>
-        <div class="divide40"></div>
-        <h2 class="section-title">My Services</h2>
-        <div class="divide10"></div>
-        <div class="services-1">
-          <div class="row">
-            <div class="col-md-4 col-sm-6">
-              <div class="icon"> <i class="budicon-heart icn"></i> </div>
-              <!-- /.icon -->
-              <div class="text">
-                <h5>Wedding Photography</h5>
-                <p>Nulla vitae libero, a pharetra augue. Integer posuere a ante venenatis condimentum. Donec id elit non mi.</p>
-              </div>
-              <!-- /.text --> 
-            </div>
-            <!-- /column -->
-            <div class="col-md-4 col-sm-6">
-              <div class="icon"> <i class="budicon-gender-female icn"></i> </div>
-              <!-- /.icon -->
-              <div class="text">
-                <h5>Birth Photography</h5>
-                <p>Vestibulum id ligula porta euismod semper. Aenean lacinia bibendum vestibulum. Cum sociis natoque.</p>
-              </div>
-              <!-- /.text --> 
-            </div>
-            <!-- /column -->
-            <div class="col-md-4 col-sm-6">
-              <div class="icon"> <i class="budicon-meal icn"></i> </div>
-              <!-- /.icon -->
-              <div class="text">
-                <h5>Food Photography</h5>
-                <p>Aenean eu leo quam. Pellente ornare sem lacinia quam venenatis vestibulum sagittis montes, nascetur ridiculus mus.</p>
-              </div>
-              <!-- /.text --> 
-            </div>
-            <!-- /column --> 
-			<div class="divide30"></div>
-            <div class="col-md-4 col-sm-6">
-              <div class="icon"> <i class="budicon-bag icn"></i> </div>
-              <!-- /.icon -->
-              <div class="text">
-                <h5>Fashion Photography</h5>
-                <p>Fusce dapibus commodo, tortor mauris condimentum nibh, ut fermentum massa. Maecenas sed diam eget risus.</p>
-              </div>
-              <!-- /.text --> 
-            </div>
-            <!-- /column -->
-            <div class="col-md-4 col-sm-6">
-              <div class="icon"> <i class="budicon-diamond icn"></i> </div>
-              <!-- /.icon -->
-              <div class="text">
-                <h5>Jewelery Photography</h5>
-                <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Vestibulum id ligula porta felis sit amet non magna sit amet.</p>
-              </div>
-              <!-- /.text --> 
-            </div>
-            <!-- /column -->
-            <div class="col-md-4 col-sm-6">
-              <div class="icon"> <i class="budicon-cocktail icn"></i> </div>
-              <!-- /.icon -->
-              <div class="text">
-                <h5>Drink Photography</h5>
-                <p>Morbi leo risus, porta ac consectetur ac at eros. Cras mattis consectetur purus. Cras mattis consectetur purus.</p>
-              </div>
-              <!-- /.text --> 
-            </div>
-            <!-- /column --> 
-          </div>
-          <!-- /.row --> 
-        </div>
-        <!-- /.services --> 
-        
       </div>
       <!-- /.box --> 
     </section>
     <!-- /#about -->
+        <section id="author">
+          <div class="box">
+            <h2 class="section-title">Доктор Ю. Чикуров. Человек придумавший "Девяточку"?</h2>
+            <div class="row">
+              <div class="col-md-7 col-md-pull-5 col-sm-12">
+                <p class="lead">Доктор Ю. Чикуров, кандидат медицинских наук, специализации в области неврологии, остеопатии и психологии</p>
+                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Fusce dapibus, tellus. </p>
+                <p>Tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id elit. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+                <p>Fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Etiam porta sem malesuada magna mollis euismod. Aenean lacinia bibendum nulla.</p>
+              </div>
+              <!-- /column -->
+              <div class="col-md-5 col-md-push-7 col-sm-12">
+                <figure class="frame"><img src="<?php bloginfo('template_url'); ?>/style/images/art/chikurov.jpg" alt="" /></figure>
+              </div>
+              <!-- /column -->
+            </div>
+            <!-- /.row -->
+            <div class="clearfix"></div>
+          </div>
+          <!-- /.box --> 
+        </section>
+        <!-- /#author -->
     
     <section id="contact">
       <div class="box">
