c8c85ed4830fa454a79b79ee2c7dc1f986272118
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-08-07 15:22:04 +0300

    nine-22

--- wp-content/themes/marakata/functions.php
+++ wp-content/themes/marakata/functions.php
@@ -3,18 +3,18 @@
   add_action('template_redirect', 'kama_login_redirect');
   function kama_login_redirect(){
       if(!is_user_logged_in()){
-        if( strpos($_SERVER['REQUEST_URI'], 'login')!==false )
-          $loc = '/';
-        elseif( strpos($_SERVER['REQUEST_URI'], 'wp-login')!==false )
-          $loc = '/wp-login.php';
-        elseif( strpos($_SERVER['REQUEST_URI'], 'admin')!==false )
-          $loc = '/wp-admin/';
-              elseif( strpos($_SERVER['REQUEST_URI'], 'registration')!==false )
-          $loc = 'wp-login.php?action=register';
-        if( $loc ){
-          header( 'Location: '.get_option('site_url').$loc, true, 303 );
-          exit;
-        }
+    if( strpos($_SERVER['REQUEST_URI'], 'login')!==false )
+      $loc = '/';
+    elseif( strpos($_SERVER['REQUEST_URI'], 'wp-login')!==false )
+      $loc = 'wp-login.php?action=login';
+    elseif( strpos($_SERVER['REQUEST_URI'], 'admin')!==false )
+      $loc = '/wp-admin/';
+          elseif( strpos($_SERVER['REQUEST_URI'], 'registration')!==false )
+      $loc = 'wp-login.php?action=register';
+    if( $loc ){
+      header( 'Location: '.get_option('site_url').$loc, true, 303 );
+      exit;
+    }
       }
   }
 
@@ -26,4 +26,14 @@
               return home_url('/wp-admin/');
       return home_url();
   }
+
+  //fix for cookie error while login.
+  setcookie(TEST_COOKIE, 'WP Cookie check', 0, COOKIEPATH, COOKIE_DOMAIN); 
+  if ( SITECOOKIEPATH != COOKIEPATH ) 
+  setcookie(TEST_COOKIE, 'WP Cookie check', 0, SITECOOKIEPATH, COOKIE_DOMAIN);
+
+  /* Отключаем админ панель для всех, кроме администраторов. */
+  if (!current_user_can('administrator')):
+    show_admin_bar(false);
+  endif;
 ?>
