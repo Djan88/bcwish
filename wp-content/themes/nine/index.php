25bb769761e16557aa8b163805cbbee95f433cd0
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-08-10 14:24:21 +0300

    nine_name-6

--- wp-content/themes/nine/index.php
+++ wp-content/themes/nine/index.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="">
 <meta name="author" content="">
-<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.ico">
+<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/style/images/favicon.ico">
 <title><?php wp_title(''); ?></title>
 <!-- Bootstrap core CSS -->
 <link href="<?php bloginfo('template_url'); ?>/style/css/bootstrap.min.css" rel="stylesheet">
