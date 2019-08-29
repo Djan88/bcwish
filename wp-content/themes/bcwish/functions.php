2f6bf0917777787c3ce4f719b01b7989462bbdeb
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-08-29 15:22:15 +0300

    newWM-34

--- wp-content/themes/bcwish/functions.php
+++ wp-content/themes/bcwish/functions.php
@@ -6,7 +6,7 @@ die();
 
 if(!current_user_can('manage_options')){
 add_action('admin_init','users_redirect');
-// add_filter('login_redirect', 'users_redirect');
+  add_filter('login_redirect', 'users_redirect');
 }
 
 //fix for cookie error while login.
