04e381a87560ad2cf452f32d3c7dc8b497d03850
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-09-09 21:10:39 +0300

    interface-113

--- wp-content/themes/bcwish/index.php
+++ wp-content/themes/bcwish/index.php
@@ -646,8 +646,8 @@
   <script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
   <script src="<?php bloginfo('template_url'); ?>/js/JCrop.min.js"></script>
   <script src="<?php bloginfo('template_url'); ?>/js/exif.js"></script>
-  <script src="<?php bloginfo('template_url'); ?>/js/script.js"></script>
   <script src="<?php bloginfo('template_url'); ?>/js/protocols.js"></script>
+  <script src="<?php bloginfo('template_url'); ?>/js/script.js"></script>
 
 </body>
 </html>
