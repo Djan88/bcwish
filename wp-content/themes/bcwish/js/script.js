02e416341f1b5da82b9646b23b2a101066d6b1a3
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-09-09 19:56:26 +0300

    interface-93

--- wp-content/themes/bcwish/js/script.js
+++ wp-content/themes/bcwish/js/script.js
@@ -17,18 +17,12 @@ jQuery(function() {
     jQuery('.register_form').removeClass('hidden').addClass('bounceInUp');
   });
 
+//Скрываем возможно загруженное изображение
+  jQuery('.wizard_returned').find('img:first-child').addClass('returned hidden');
+  croppedImg = jQuery('.wizard_returned').children()[0];
 
 
-
-  //Dragging elems
-  jQuery( ".draggable, .ring" ).draggable({
-      snap: false
-  });
-
-  //Изменение размера круга
-  jQuery( ".ring" ).resizable({
-    aspectRatio: 1/1;
-  });
+  
 
 // Если фото уже загружено
   if (croppedImg && croppedImg.hasAttribute('src')) {
