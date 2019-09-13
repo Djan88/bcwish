594bce4f112132597068d14c0a7f49529983f2fa
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-09-13 15:51:14 +0300

    final_screen_21

--- wp-content/themes/bcwish/js/protocols.js
+++ wp-content/themes/bcwish/js/protocols.js
@@ -8,6 +8,12 @@ jQuery(function() {
       protocol,
       checkPoints,
       pointsStatus = true,
+      reloadTime = 0,
+      reloadTime1 = 0,
+      d12Val = 0,
+      cur_animation_val = 0,
+      rotateVal = 0,
+      count_animation = 1,
       supportsStorage = function(){
           try {
               return 'localStorage' in window && window['localStorage'] !== null;
@@ -49,8 +55,99 @@ jQuery(function() {
     }
   });
 
-  var protocol = localStorage.getItem('cur_protocol');
-  console.log(protocol);
+
+  v1 = function(){
+    jQuery('.status').removeClass('hidden');
+    jQuery('.status_pahaze_all').text('3');
+    localStorage.setItem('pausedPhases', '3');
+    localStorage.setItem('pausedProtName', 'Универсальный протокол');
+    jQuery('.wizard_percent').text('0%');
+    reloadTime = 0;
+    reloadTime1 = 0;
+    d12Val = 0;
+    cur_animation_val = 0;
+    rotateVal = 0;
+    count_animation = 1;
+    phaseOne = setInterval(function(){
+      if (count_animation <= 344){
+        jQuery('.zone_v5, .zone_d5, .zone_d6').css({
+            color: 'transparent',
+            borderColor: 'transparent',
+            opacity: 0.8,
+            borderWidth: '1px',
+            paddingTop: '4px',
+            transform: 'rotate('+rotateVal+'deg) scale(1)',
+            background: '#fff url(/wp-content/themes/bcwish/img/superdisfunction.png) 0 0/100% no-repeat',
+            zIndex: '1000'
+        });
+        jQuery('.zone_v-').css({
+            color: 'transparent',
+            borderColor: 'transparent',
+            opacity: 0.8,
+            borderWidth: '1px',
+            paddingTop: '4px',
+            background: 'url(/wp-content/themes/bcwish/img/vig_.png) 0 0/100% no-repeat',
+            transform: 'scale(1)',
+            zIndex: '1000'
+        });
+        jQuery('.zone_ring')
+          .removeClass('hidden')
+          .css({
+            opacity: 0.8,
+            transform: 'scale(1)',
+            background: '#fff url(/wp-content/themes/bcwish/img/lovushka.jpg) 0 0/100% no-repeat',
+            transform: 'rotate(-'+d12Val+'deg)',
+            borderWidth: '1px',
+            borderColor: 'transparent'
+          });
+        count_animation += 1;
+        rotateVal += 1.5;
+        if(count_animation <= 120){
+            cur_animation_val += 1.5;
+            d12Val+= 9;
+            jQuery('.ring').css('transform', 'rotate('+cur_animation_val+'deg) scale(1)');
+        } else if (count_animation >= 120 && count_animation <= 228){
+            cur_animation_val -= 1.5;
+            d12Val+= 9;
+            jQuery('.zone_ring').css('transform', 'rotate(-'+d12Val+'deg)');
+            jQuery('.ring').css('transform', 'rotate('+cur_animation_val+'deg) scale(1)');
+        } else if (count_animation >= 228 && count_animation <= 292){
+            cur_animation_val -= 1.5;
+            d12Val+= 9;
+            jQuery('.ring').css('transform', 'rotate('+cur_animation_val+'deg) scale(1)');
+            jQuery('.zone_ring').css('transform', 'rotate('+d12Val+'deg)');
+            jQuery('.zone_ring').css('background', '#fff url(/wp-content/themes/bcwish/img/daemon.png) 0 0/100% no-repeat');
+        } else if (count_animation >= 292 && count_animation <= 344){
+            cur_animation_val += 1.5;
+            d12Val+= 9;
+            jQuery('.ring').css('transform', 'rotate('+cur_animation_val+'deg) scale(1)');
+            jQuery('.zone_ring').css('transform', 'rotate('+d12Val+'deg)');
+            jQuery('.zone_ring').css('background', '#fff url(/wp-content/themes/bcwish/img/daemon.png) 0 0/100% no-repeat');
+        } else {
+            d12Val+= 9;
+            cur_animation_val += 1.5;
+            jQuery('.ring').css('transform', 'rotate('+cur_animation_val+'deg) scale(1)');
+            jQuery('.zone_ring').css('transform', 'rotate('+d12Val+'deg)');
+            jQuery('.zone_ring').css('background', '#fff url(/wp-content/themes/bcwish/img/daemon.png) 0 0/100% no-repeat');
+        }
+      } else {
+        clearInterval(phaseOne);
+        count_animation = 1;
+        jQuery('.zone_v5, .zone_d5, .zone_d6, .zone_v-').css({
+            background: 'rgba(255,255,255, 0.5)',
+            color: 'red',
+            borderColor: 'red',
+            opacity: 1,
+            transform: 'scale(0.5)',
+            borderWidth: '2px',
+            paddingTop: '5px',
+            zIndex: '1'
+        });
+        jQuery('.ring').css('transform', 'rotate(0deg) scale(1)');
+        jQuery('.zone_ring').css('transform', 'rotate(0deg)');
+      }
+    }, 250);
+  }
   
   checkPoints = function(){
     jQuery('.zone_movable').each(function() {
@@ -66,17 +163,44 @@ jQuery(function() {
     if(pointsStatus == false){
       swal("Не все зоны перенесены!", "Перед началом процедуры необходимо перенести на фото все зоны.", "info");
       pointsStatus = true;
-      console.log('zones_alert');
     } else {
+      jQuery('.wizard_heading').text('Программа выполняется.');
       jQuery(this).addClass('wizard_play_started');
       jQuery('.wizard_start_icon').addClass('hidden');
       jQuery('.wizard_stop_icon, .wizard_percent').fadeIn(500).removeClass('hidden');
+      jQuery('.zone').css('background', 'rgba(83,35,69, 0.4)');
+      var protocol = localStorage.getItem('cur_protocol');
+      console.log(protocol);
+
+      if (protocol == 'v1') {
+        v1();
+        jQuery('.status_title').text('Протокол V1');
+      } else if (protocol == 'v2') {
+        v2();
+        jQuery('.status_title').text('Протокол V2-5');
+      } else if (protocol == 'v3') {
+        v3();
+        jQuery('.status_title').text('Протокол V3-4');
+      } else if (protocol == 'v4') {
+        v4();
+        jQuery('.status_title').text('Протокол V4-3');
+      } else if (protocol == 'v5') {
+        v5();
+        jQuery('.status_title').text('Протокол V5-2');
+      } else if (protocol == 'solis') {
+        solis();
+        jQuery('.status_title').text('Протокол Solis');
+      } else if (protocol == 'drenag') {
+        drenag();
+        jQuery('.status_title').text('Дренажный протокол');
+      } else if (protocol == 'universal') {
+        universal();
+        jQuery('.status_title').text('Универсальный протокол');
+      } else if (protocol == 'visceral') {
+        mmt();
+        jQuery('.status_title').text('Висцеральный протокол');
+      }
     }
   });
-
-
-  v1 = function(){
-    console.log('Фаза 1');
-  }
 
 });
