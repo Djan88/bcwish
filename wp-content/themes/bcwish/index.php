121e2986302cc7ad99aaa0c3d621724cb1fe6dfd
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-09-16 14:18:50 +0300

    final_screen_75

--- wp-content/themes/bcwish/index.php
+++ wp-content/themes/bcwish/index.php
@@ -202,7 +202,7 @@
     <!--==========================
       Main Section
     ============================-->
-    <section id="services" class="wow bounceInUp">
+    <section id="services wm_start" class="wow bounceInUp unopacity">
       <div class="container container_main">
 
         <header class="section-header">
