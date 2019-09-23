6dfdd62a75a93318eacde612cd5c54d2a8f1e3d3
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-09-23 18:43:38 +0300

    v9

--- wp-content/themes/bcwish/js/protocols.js
+++ wp-content/themes/bcwish/js/protocols.js
@@ -148,6 +148,55 @@ jQuery(function() {
     }
   });
 
+  v3_5_4 = function(){
+    jQuery('.wizard_heading').text('Выполняется протокол "V 3 — V 4"');
+    jQuery('.wizard_percent').text('78%');
+    reloadTime = 0;
+    reloadTime1 = 0;
+    d12Val = 0;
+    rotateVal = 0;
+    jQuery('.ring').addClass('hidden');
+    jQuery('.ring, .zone_ring').css('transform', 'rotate(0deg)');
+  //Этап 5-2-4
+    cur_animation_val = 320;
+    count_animation = 1;
+    phaseSeven_one = setInterval(function(){
+        if (count_animation <= 40){
+          cur_animation_val += 1.5;
+          jQuery('.zone_v1, .zone_v3, .zone_v4').css({
+            background: '#fff url(/wp-content/themes/bcwish/img/mo_right.png) center center/100% no-repeat',
+            transform: 'rotate(-'+cur_animation_val+'deg) scale(1.5)',
+            color: 'transparent',
+            borderColor: 'transparent',
+            opacity: 0.8,
+            borderWidth: '1px',
+            paddingTop: '4px',
+            zIndex: '1000'
+          });
+          count_animation += 1;
+        } else if(count_animation <= 57) {
+            count_animation += 1;
+        } else {
+          clearInterval(phaseSeven_one);
+          count_animation = 1;
+          jQuery('.zone_v1, .zone_v3, .zone_v4').css({
+            background: '#fff',
+            color: '#413e66',
+            borderColor: '#413e66',
+            transform: 'rotate(-'+0+'deg) scale(1)',
+            paddingTop: '2px',
+            zIndex: '1'
+          });
+          if (pausedStatus == true) {
+            localStorage.setItem('paused', 'v3_6_1');
+            endNow();
+          } else {
+            v3_6_1();
+          } 
+        }
+    }, 1000);
+  }
+
   v3_5_3 = function(){
     jQuery('.wizard_heading').text('Выполняется протокол "V 3 — V 4"');
     jQuery('.wizard_percent').text('78%');
