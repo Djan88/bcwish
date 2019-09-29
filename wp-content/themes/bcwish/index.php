bb4a3dcc6c697f17ace1cedf51f6c205359226a8
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-09-29 12:59:08 +0300

    user-20

--- wp-content/themes/bcwish/index.php
+++ wp-content/themes/bcwish/index.php
@@ -294,12 +294,11 @@
                       <p>
                         «Краниальный протокол» - «используется при актуальности зоны головы, а также для терапевтической дефрагментации ума»
                       </p>
-                      <?php
-                      if ($new_registred == 1) {
-                        echo '<p><button class="btn wizard_blue wizard_disabled" data-toggle="popover" data-placement="right" title="Протокол недоступен!" data-content="Данный протокол доступен пользователям с действительным доступом">Активировать V1</button></p>';
-                      } else {
-                        echo '<p><button class="btn wizard_blue wizard_protocol wizard_protocol_1 wizard_prot_1 wow bounceInUp" data-wow-duration="1.4s">Активировать V1</button></p>';
-                      }
+                      <?php if ($new_registred == 1) { ?>
+                        <p><button class="btn wizard_blue wizard_disabled wow bounceInUp" data-wow-duration="1.4s" data-toggle="popover" data-placement="right" title="Протокол недоступен!" data-content="Данный протокол доступен пользователям с действительным доступом">Активировать V1</button></p>
+                      <?php } else { ?>
+                        <p><button class="btn wizard_blue wizard_protocol wizard_protocol_1 wizard_prot_1 wow bounceInUp" data-wow-duration="1.4s">Активировать V1</button></p>
+                      <?php } ?>
                       ?>
                       <!-- <p><button class="btn wizard_blue wizard_protocol wizard_protocol_1 wizard_prot_1 wow bounceInUp" data-wow-duration="1.4s">Активировать V1</button></p> -->
                     </div>
