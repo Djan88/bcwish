fc96a6bab6bc86c2e944dd767b2688c7b30d06a1
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-04-09 16:30:08 +0300

    duos_5

--- wp-content/themes/bcwish/index.php
+++ wp-content/themes/bcwish/index.php
@@ -321,7 +321,10 @@
                 </div>
               </div>
               <div class="row wizard_operation">
-                <div class="col-md-12 wizard_way hidden">
+                <div class="col-md-6 wizard_way hidden">
+                  шаблоны
+                </div>
+                <div class="col-md-6 wizard_way hidden">
                   <!-- <button class="btn btn-success btn_diag wow bounceInUp back-to-top_2" data-wow-duration="1.4s" data-wow-delay="0.4s">Диагностика</button> -->
                   <div class="row uploaded_pics_wrap draggable_photo wow bounceInUp back-to-top_2">
                     <div class="uploaded_pics_wrapper uploaded_pics_wrapper_1 draggable_photo wow bounceInUp back-to-top_1" data-wow-duration="1.4s" data-wow-delay="0.4s">
